a33c6eb46ad68520d58267d51e89e3bf34114b08
MusimathicsLab <dev.cirillo@gmail.com>
2023-06-05 15:37:04 +0200

    added str2bool method

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,14 @@
+from typing import Final
+import argparse
+
 # test utils
 from utils._test import create_test_name
 from utils._test import test_check
+from utils._test import evaluate_check
 
 # file and directory utils
 from utils._file import create_folders
+from utils._file import delete_checkpoints
 
 # metric utils
 from utils._metric import save_result
@@ -12,5 +17,15 @@ from utils._metric import save_result
 from utils._logger import setup_logger
 from utils._logger import close_loggers
 
-from typing import Final
 SEPARATOR: Final = '\n------------------------------------\n'
+
+
+def str2bool(v):
+    if isinstance(v, bool):
+        return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
