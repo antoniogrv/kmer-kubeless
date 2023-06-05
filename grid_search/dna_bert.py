3e4b942a281631dc397b30fb52807c310f5fd2cb
MusimathicsLab <dev.cirillo@gmail.com>
2023-06-05 15:35:39 +0200

    added grid search parameter

--- grid_search/dna_bert.py
+++ grid_search/dna_bert.py
@@ -2,11 +2,11 @@ import os
 
 if __name__ == '__main__':
     for rnn_layer in ['lstm', 'gru']:
-        for n_attention_heads in [1, 2, 4, 8, 12]:
-            for hidden in [64, 128, 256, 512, 768]:
-                for n_hidden_layer in [3, 5, 7, 9, 12]:
+        for n_attention_heads in [2, 4, 8, 12]:
+            for hidden in [256, 512, 768]:
+                for n_hidden_layer in [7, 9]:
                     for n_rnn_layers in [1, 3, 5, 7]:
-                        for n_words in [30, 40, 50]:
+                        for n_words in [30]:
                             command: str = f'python3 {os.path.join(os.getcwd(), "main.py ")}' \
                                            f'-len_read 150 ' \
                                            f'-len_overlap 0 ' \
@@ -14,13 +14,14 @@ if __name__ == '__main__':
                                            f'-n_words {n_words} ' \
                                            f'-batch 1024 ' \
                                            f'-model_selected dna_bert ' \
-                                           f'-tokenizer_selected dna_bert ' \
+                                           f'-tokenizer_selected dna_bert_n ' \
                                            f'-hidden_size {hidden} ' \
                                            f'-dropout 0.5 ' \
                                            f'-n_attention_heads {n_attention_heads} ' \
                                            f'-n_beams 1 ' \
                                            f'-n_hidden_layers {n_hidden_layer} ' \
                                            f'-rnn {rnn_layer} ' \
-                                           f'-n_rnn_layers {n_rnn_layers}'
+                                           f'-n_rnn_layers {n_rnn_layers} ' \
+                                           f'-grid_search True'
 
                             os.system(command)
