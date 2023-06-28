6ecdcdbe9bb2ea3303fa38c6e36ca48953bee5f5
MusimathicsLab <dev.cirillo@gmail.com>
2023-06-29 00:36:44 +0200

    changed name of package

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,4 @@
-from dataset.dataset import MyDataset
+from dataset._dataset import MyDataset
 
 from dataset._transcript_dataset import TranscriptDataset
 from dataset._fusion_dataset import FusionDataset
