aeb9246d431f89c7b3a6efceafb4735879983c1b
MusimathicsLab <dev.cirillo@gmail.com>
2023-06-29 00:43:03 +0200

    updated hyperparameters

--- grid_search/dna_bert.py
+++ grid_search/dna_bert.py
@@ -2,17 +2,17 @@ import os
 
 if __name__ == '__main__':
     for rnn_layer in ['lstm', 'gru']:
-        for n_attention_heads in [2, 4, 8, 12]:
-            for hidden in [256, 512, 768]:
-                for n_hidden_layer in [7, 9]:
-                    for n_rnn_layers in [1, 3, 5, 7]:
-                        for n_words in [30]:
+        for n_attention_heads in [1, 2, 4, 8, 12]:
+            for hidden in [768, 1024]:
+                for n_hidden_layer in [7, 9, 12]:
+                    for n_rnn_layers in [1]:
+                        for n_words in [20, 30]:
                             command: str = f'python3 {os.path.join(os.getcwd(), "train_gene_classifier.py ")}' \
                                            f'-len_read 150 ' \
                                            f'-len_overlap 0 ' \
                                            f'-len_kmer 6 ' \
                                            f'-n_words {n_words} ' \
-                                           f'-batch 1024 ' \
+                                           f'-batch 512 ' \
                                            f'-model_selected dna_bert ' \
                                            f'-tokenizer_selected dna_bert_n ' \
                                            f'-hidden_size {hidden} ' \
