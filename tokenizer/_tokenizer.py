36246981796cb4166b1b311be69df1bb4ba2af94
MusimathicsLab <dev.cirillo@gmail.com>
2023-06-29 17:12:14 +0200

    added load_vocab method

--- tokenizer/_tokenizer.py
+++ tokenizer/_tokenizer.py
@@ -5,6 +5,17 @@ from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.tokenization_bert import BasicTokenizer
 
 
+def load_vocab(vocab_file):
+    """Loads a vocabulary file into a dictionary."""
+    vocab = collections.OrderedDict()
+    with open(vocab_file, "r", encoding="utf-8") as reader:
+        tokens = reader.readlines()
+    for index, token in enumerate(tokens):
+        token = token.rstrip("\n")
+        vocab[token] = index
+    return vocab
+
+
 class MyDNATokenizer(PreTrainedTokenizer):
     r"""
     Constructs a BertTokenizer.
