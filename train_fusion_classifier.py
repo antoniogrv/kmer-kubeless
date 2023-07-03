8aa586504f4ea55c0c23dbe96d26a3ed8520ec44
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-03 19:31:49 +0200

    completed flow of training and evaluation added

--- train_fusion_classifier.py
+++ train_fusion_classifier.py
@@ -1,152 +1,306 @@
+from typing import Optional
 from typing import Final
 from typing import Dict
 
-import numpy as np
-import argparse
+from dotenv import load_dotenv
 import logging
 import torch
 import os
 
+from tokenizer import MyDNATokenizer
+from tokenizer import DNABertTokenizer
+
 from dataset import FusionDataset
 from torch.utils.data import DataLoader
 
-from train_gene_classifier import define_input_args_model_hyperparameters
-from train_gene_classifier import check_gene_classifier_hyperparameters
-from train_gene_classifier import init_hyperparameters_dict
+from model import MyModel
+from model import FusionClassifier
+
+from torch.optim import AdamW
+from sklearn.metrics import classification_report
+
 from train_gene_classifier import train_gene_classifier
 
 from utils import SEPARATOR
-from utils import str2bool
-from utils import create_test_name
-from utils import test_check
-from utils import create_folders
+from utils import define_fusion_classifier_inputs
+from utils import create_test_id
+from utils import init_test
 from utils import setup_logger
-from utils import save_result
+from utils import evaluate_weights
 from utils import close_loggers
+from utils import save_result
 
-TASK: Final = 'gene_fusion'
 
-
-def main(
+def train_fusion_classifier(
         len_read: int,
-        len_overlap: int,
         len_kmer: int,
         n_words: int,
         tokenizer_selected: str,
-        batch_size: int,
+        n_fusion: int,
+        classification_type: str,
         gc_model_selected: str,
-        gc_hyperparameter: Dict[str, any],
+        gc_hyperparameters: Dict[str, any],
+        gc_batch_size: int,
+        gc_re_train: bool,
         model_selected: str,
-        hyperparameter: Dict[str, any],
-        grid_search: bool
+        hyperparameters: Dict[str, any],
+        batch_size: int,
+        re_train: bool,
+        grid_search: bool,
 ):
-    """
-    # generate test name
-    test_name: str = create_test_name(
+    # execute train_gene_classifier
+    gc_model_path: str = train_gene_classifier(
         len_read=len_read,
-        len_overlap=len_overlap,
         len_kmer=len_kmer,
         n_words=n_words,
         tokenizer_selected=tokenizer_selected,
-        hyperparameter={**gc_hyperparameter, **hyperparameter}
+        model_selected=gc_model_selected,
+        hyperparameters=gc_hyperparameters,
+        batch_size=gc_batch_size,
+        re_train=gc_re_train,
+        grid_search=grid_search
     )
-    print(f'Test name: {test_name}')
-
-    # check if this configuration is already tested
-    if not test_check(task=TASK, model_name=model_selected, parent_name=test_name):
-        print(f'Initialization of the test...')
-        # check if gene classifier is trained, otherwise train it
-        train_gene_classifier(
-            len_read=len_read,
-            len_overlap=len_overlap,
+    # get value from .env
+    root_dir: Final = os.getenv('ROOT_LOCAL_DIR')
+    # init tokenizer
+    tokenizer = Optional[MyDNATokenizer]
+    if tokenizer_selected == 'dna_bert':
+        tokenizer = DNABertTokenizer(
+            root_dir=root_dir,
             len_kmer=len_kmer,
-            n_words=n_words,
-            model_selected=gc_model_selected,
-            tokenizer_selected=tokenizer_selected,
-            batch_size=batch_size,
-            hyperparameter=gc_hyperparameter,
-            grid_search=True
+            add_n=False
         )
-        # create folders and get path
-        log_path, model_path = create_folders(
-            task=TASK,
-            model_name=model_selected,
-            parent_name=test_name
+    elif tokenizer_selected == 'dna_bert_n':
+        tokenizer = DNABertTokenizer(
+            root_dir=root_dir,
+            len_kmer=len_kmer,
+            add_n=True
         )
-        # init loggers
-        logger: logging.Logger = setup_logger(
-            'logger',
-            os.path.join(log_path, 'logger.log')
-        )
-        train_logger: logging.Logger = setup_logger(
-            'train',
-            os.path.join(log_path, 'train.log')
-        )
-    """
-
-    train_dataset = FusionDataset(
-        root_dir=os.path.join(os.getcwd(), 'data'),
+    # generate test id
+    test_id: str = create_test_id(
         len_read=len_read,
         len_kmer=len_kmer,
         n_words=n_words,
-        dataset_type='train'
+        tokenizer=tokenizer,
+        hyperparameters={**gc_hyperparameters, **hyperparameters}
+    )
+    # create dataset configuration
+    dataset_conf: Dict[str, any] = FusionDataset.create_conf(
+        transcript_dir=os.getenv('TRANSCRIPT_LOCAL_DIR'),
+        len_read=len_read,
+        len_kmer=len_kmer,
+        n_words=n_words,
+        tokenizer=tokenizer,
+        n_fusion=n_fusion,
+        classification_type=classification_type
+    )
+    # get global variables
+    task: str = os.getenv('FUSION_CLASSIFIER_TASK')
+    result_dir: str = os.path.join(os.getcwd(), os.getenv('RESULTS_LOCAL_DIR'))
+    model_name: str = os.getenv('MODEL_NAME')
+    # init test
+    parent_dir, test_dir, log_dir, model_dir, model_path = init_test(
+        result_dir=result_dir,
+        task=os.path.join(task, classification_type),
+        model_selected=model_selected,
+        test_id=test_id,
+        model_name=model_name,
+        re_train=re_train
+    )
+
+    # if the model has not yet been trained
+    if not os.path.exists(model_path):
+        # init loggers
+        logger: logging.Logger = setup_logger(
+            'logger',
+            os.path.join(log_dir, 'logger.log')
+        )
+        train_logger: logging.Logger = setup_logger(
+            'train',
+            os.path.join(log_dir, 'train.log')
+        )
+
+        # load train and validation dataset
+        train_dataset = FusionDataset(
+            root_dir=root_dir,
+            conf=dataset_conf,
+            dataset_type='train'
+        )
+        val_dataset = FusionDataset(
+            root_dir=root_dir,
+            conf=dataset_conf,
+            dataset_type='val'
+        )
+
+        # log information
+        logger.info(f'Read len: {len_read}')
+        logger.info(f'Kmers len: {len_kmer}')
+        logger.info(f'Words inside a sentence: {n_words}')
+        logger.info(f'Tokenizer used: {tokenizer_selected}')
+        logger.info(f'No. fusions generated for each gene: {n_fusion}')
+        logger.info(SEPARATOR)
+        logger.info(f'Number of train sentences: {len(train_dataset)}')
+        logger.info(f'Number of val sentences: {len(val_dataset)}')
+        logger.info(f'Number of class: {train_dataset.classes()}')
+        logger.info(f'Batch size: {batch_size}')
+        logger.info(SEPARATOR)
+        logger.info(f'Number of train sentences: {len(train_dataset)}')
+        logger.info(f'Number of val sentences: {len(val_dataset)}')
+        logger.info(f'Number of class: {train_dataset.classes()}')
+        logger.info(f'Batch size: {batch_size}')
+        logger.info(SEPARATOR)
+        # print dataset status
+        logger.info('No. records train set')
+        logger.info(train_dataset.print_dataset_status())
+        logger.info('No. records val set')
+        logger.info(val_dataset.print_dataset_status())
+
+        # load train and validation dataloader
+        train_loader: DataLoader = DataLoader(
+            train_dataset,
+            batch_size=batch_size,
+            shuffle=True
+        )
+        val_loader: DataLoader = DataLoader(
+            val_dataset,
+            batch_size=batch_size,
+            shuffle=True
+        )
+
+        # set device gpu if cuda is available
+        device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        # evaluating weights for criterion function
+        class_weights: torch.Tensor = evaluate_weights(train_dataset).to(device)
+
+        # update hyperparameter
+        hyperparameters['gene_classifier'] = gc_model_path
+        n_kmers: int = len_read - len_kmer + 1
+        hyperparameters['n_sentences'] = n_kmers - n_words + 1
+        if classification_type == 'fusion':
+            hyperparameters['n_classes'] = 2
+
+        # define model
+        model: MyModel = FusionClassifier(
+            model_dir=model_dir,
+            model_name=model_name,
+            hyperparameter=hyperparameters,
+            weights=class_weights
+        )
+
+        # log model hyper parameters
+        logger.info('Gene classifier hyperparameter')
+        logger.info(model.gene_classifier.print_hyperparameter())
+        logger.info('Fusion classifier hyperparameter')
+        logger.info(model.print_hyperparameter())
+
+        # init optimizer
+        optimizer = AdamW(
+            model.parameters(),
+            lr=5e-5,
+            eps=1e-8,
+            betas=(0.9, 0.999)
+        )
+
+        # put model on device available
+        model.to(device)
+        # train it
+        model.train_model(
+            train_loader=train_loader,
+            optimizer=optimizer,
+            device=device,
+            epochs=1000,
+            evaluation=True,
+            val_loader=val_loader,
+            logger=train_logger
+        )
+
+        # close loggers
+        close_loggers([train_logger, logger])
+        del train_logger
+        del logger
+
+    # if the model is already trained and the grid search parameter is set to true then stop
+    elif grid_search:
+        return model_path
+
+    # init loggers
+    logger: logging.Logger = setup_logger(
+        'logger',
+        os.path.join(log_dir, 'logger.log')
+    )
+    result_logger: logging.Logger = setup_logger(
+        'result',
+        os.path.join(test_dir, 'result.log')
+    )
+
+    # load test dataset
+    test_dataset = FusionDataset(
+        root_dir=root_dir,
+        conf=dataset_conf,
+        dataset_type='test'
+    )
+
+    # log test dataset status
+    logger.info('No. records test set')
+    logger.info(test_dataset.print_dataset_status())
+
+    # load model
+    model: MyModel = torch.load(model_path)
+    # set device gpu if cuda is available
+    device: torch.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # set model on gpu
+    model.to(device)
+
+    # create test data loader
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        shuffle=True
+    )
+    # test model
+    y_true, y_pred = model.predict(
+        test_loader=test_loader,
+        device=device
+    )
+
+    # log classification report
+    report: str = classification_report(
+        y_true,
+        y_pred,
+        digits=3,
+        zero_division=1,
+        target_names=test_dataset.get_labels_dict().keys()
+    )
+    result_logger.info(report)
+
+    # close loggers
+    close_loggers([logger, result_logger])
+    del logger
+    del result_logger
+
+    # save result
+    save_result(
+        result_csv_path=os.path.join(parent_dir, 'results.csv'),
+        len_read=len_read,
+        len_kmer=len_kmer,
+        n_words=n_words,
+        tokenizer_selected=tokenizer_selected,
+        hyperparameter=model.hyperparameter,
+        y_true=y_true,
+        y_pred=y_pred
     )
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    # define inputs for this script
+    __args, __gc_hyperparameters, __hyperparameters = define_fusion_classifier_inputs()
 
-    parser.add_argument('-len_read', dest='len_read', action='store',
-                        type=int, default=150, help='define length of reads')
-    parser.add_argument('-len_overlap', dest='len_overlap', action='store',
-                        type=int, default=0, help='define length of overlapping between reads')
-    parser.add_argument('-len_kmer', dest='len_kmer', action='store',
-                        type=int, default=6, help='define length of kmers')
-    parser.add_argument('-n_words', dest='n_words', action='store',
-                        type=int, default=30, help='number of kmers inside a sentence')
-    parser.add_argument('-tokenizer_selected', dest='tokenizer_selected', action='store',
-                        type=str, default='dna_bert_n', help='select the tokenizer to be used')
-    parser.add_argument('-batch_size', dest='batch_size', action='store',
-                        type=int, default=512, help='define batch size')
+    # load dotenv file
+    load_dotenv(dotenv_path=os.path.join(os.getcwd(), '.env'))
 
-    # gene classifier parameters
-    define_input_args_model_hyperparameters(
-        arg_parser=parser,
-        suffix='gc_'
-    )
-
-    # fusion classifier parameters
-    parser.add_argument('-dropout', dest='dropout', action='store',
-                        type=float, default=0.5, help='define value of dropout probability')
-
-    # grid search flag
-    parser.add_argument('-grid_search', dest='grid_search', action='store', type=str2bool,
-                        default=False, help='set true if this script is launching from grid_search script')
-
-    args = parser.parse_args()
-
-    # check gene classifier hyperparameters
-    check_gene_classifier_hyperparameters(
-        args_dict=vars(args),
-        suffix='gc_'
-    )
-
-    # init gene classifier hyperparameter
-    gc_config: Dict[str, any] = init_hyperparameters_dict(
-        args_dict=vars(args),
-        suffix='gc_'
-    )
-
-    main(
-        len_read=args.len_read,
-        len_overlap=args.len_overlap,
-        len_kmer=args.len_kmer,
-        n_words=args.n_words,
-        tokenizer_selected=args.tokenizer_selected,
-        batch_size=args.batch_size,
-        gc_model_selected=args.gc_model_selected,
-        gc_hyperparameter=gc_config,
-        model_selected='',
-        hyperparameter={},
-        grid_search=args.grid_search
+    # execute train_gene_classifier method
+    train_fusion_classifier(
+        **__args,
+        gc_hyperparameters=__gc_hyperparameters,
+        hyperparameters=__hyperparameters
     )
