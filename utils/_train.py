a6fb38f34dcd94180ef9a311151f2ecfdcd2e9da
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-04 22:05:07 +0200

    added handle of binary classification

--- utils/_train.py
+++ utils/_train.py
@@ -6,10 +6,14 @@ from sklearn.utils import class_weight
 
 
 def evaluate_weights(
-        train_dataset: MyDataset
+        train_dataset: MyDataset,
+        binary: bool = False
 ) -> torch.Tensor:
     y = []
     for idx, label in enumerate(train_dataset.get_dataset_status()):
         y = np.append(y, [idx] * label)
-    class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y), y=y)
+    if binary:
+        class_weights = (y == 0.).sum() / y.sum()
+    else:
+        class_weights = class_weight.compute_class_weight('balanced', classes=np.unique(y), y=y)
     return torch.tensor(class_weights, dtype=torch.float32)
