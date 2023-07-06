5e5972322aa9c0e8605ce419872470e950c27b7a
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-06 18:38:17 +0200

    removed lstm model

--- model/__init__.py
+++ model/__init__.py
@@ -4,4 +4,3 @@ from model._gene_classifier import GeneClassifier
 from model._gc_dna_bert import DNABertGeneClassifier
 
 from model._fc_gene_fusion import FCFusionClassifier
-from model._conv_gene_fusion import ConvClassifier
