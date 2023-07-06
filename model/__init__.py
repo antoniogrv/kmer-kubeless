f2b4423cda1591682e6375b79da86f13e3f0768e
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-06 02:25:16 +0200

    added geneclassifier definition

--- model/__init__.py
+++ model/__init__.py
@@ -1,6 +1,7 @@
 from model._model import MyModel
+from model._gene_classifier import GeneClassifier
 
-from model._dna_bert import DNABert
+from model._gc_dna_bert import DNABertGeneClassifier
 
 from model._fc_gene_fusion import FCFusionClassifier
 from model._conv_gene_fusion import ConvClassifier
