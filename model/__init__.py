dbd7fc240d336d1b4cee81483960fb91ee1de322
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-07 17:12:57 +0200

    added FC classifier import

--- model/__init__.py
+++ model/__init__.py
@@ -6,5 +6,6 @@ from model.gene_classifier import GCDNABertModelConfig
 from model.gene_classifier import GCDNABert
 
 from model.fusion_classifier import FCFullyConnectedModelConfig
+from model.fusion_classifier import FCFusionClassifier
 
 from model._utils import evaluate_weights
