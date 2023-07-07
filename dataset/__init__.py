3812f7b6a02dc28fa0566dbd662451738797dc53
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-07 17:12:16 +0200

    added fusion dataset imports

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -3,3 +3,6 @@ from dataset._dataset import MyDataset
 
 from dataset.transcript import TranscriptDatasetConfig
 from dataset.transcript import TranscriptDataset
+
+from dataset.fusion import FusionDatasetConfig
+from dataset.fusion import FusionDataset
