40eaed2d4de2e87ff916b1fb4f07581ac2a7e17b
MusimathicsLab <dev.cirillo@gmail.com>
2023-07-07 17:30:58 +0200

    changed name of fc

--- model/fusion_classifier/__init__.py
+++ model/fusion_classifier/__init__.py
@@ -1,2 +1,2 @@
 from model.fusion_classifier._fully_connected_config import FCFullyConnectedModelConfig
-from model.fusion_classifier._fully_connected import FCFusionClassifier
+from model.fusion_classifier._fully_connected import FCFullyConnected
